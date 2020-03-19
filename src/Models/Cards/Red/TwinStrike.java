71085ba78219b7d6005194dcc332bc7385158272
Ozge <ozgey99@gmail.com>
2020-03-19 20:12:24 +0300

    TwinStrike added

---
+++ src/Models/Cards/Red/TwinStrike.java
@@ -0,0 +1,60 @@
+package Models.Cards.Red;
+
+import Models.Cards.*;
+
+import java.util.ArrayList;
+
+public class TwinStrike extends AbstractCard {
+    private String name;
+    private String description;
+    private int cost;
+    private CardType type;
+    private CardColor color;
+    private CardRarity rarity;
+    private CardTarget target;
+    private BaseCardAttributes baseAttr;
+    private boolean usable;
+    private boolean upgradable;
+    private int repeat;
+
+    public TwinStrike(){
+        name = "Twin Strike";
+        description = "Deal 5(7) damage twice.";
+        cost = 1;
+        type = CardType.ATTACK;
+        color = CardColor.RED;
+        rarity = CardRarity.COMMON;
+        target = CardTarget.ENEMY;
+        baseAttr = new BaseCardAttributes();
+        baseAttr.damage = 5;
+        usable = true;
+        upgradable = true;
+        repeat = 2;
+    }
+
+    @Override
+    public void use(ArrayList<AbstractMonster> monsters, AbstractCharacter hero) {
+        for(int i = 0; i < repeat; i++) {
+            for (AbstractMonster m : monsters) {
+                m.changeHealth(-baseAttr.damage);
+            }
+        }
+        hero.currentEnergy -= cost;
+    }
+
+    @Override
+    public void upgrade() {
+        if(upgradable){
+            upgradable = false;
+            baseAttr.damage = 7;
+        }
+    }
+
+    @Override
+    public TwinStrike makeCopy() {
+        TwinStrike newCard = new TwinStrike();
+        newCard.upgradable = this.upgradable;
+        newCard.baseAttr.damage = this.baseAttr.damage;
+        return newCard;
+    }
+}
