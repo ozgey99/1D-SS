8650bf81ff54b157bc7e40d919631d2972b30542
Ozge <ozgey99@gmail.com>
2020-03-19 19:49:16 +0300

    SwordBoomerang added

--- src/Models/Cards/Red/SwordBoomerang.java
+++ src/Models/Cards/Red/SwordBoomerang.java
@@ -3,11 +3,13 @@ package Models.Cards.Red;
 import Models.Cards.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class IronWave extends AbstractCard {
+public class SwordBoomerang extends AbstractCard {
     private String name;
     private String description;
     private int cost;
+    private int repeat;
     private CardType type;
     private CardColor color;
     private CardRarity rarity;
@@ -16,45 +18,45 @@ public class IronWave extends AbstractCard {
     private boolean usable;
     private boolean upgradable;
 
-    public IronWave(){
-        name = "Iron Wave";
-        description = "Gain 5(7) Block. Deal 5(7) damage.";
+    public SwordBoomerang(){
+        name = "Sword Boomerang";
+        description = "Deal 3 damage to a random enemy 3(4) times.";
         cost = 1;
         type = CardType.ATTACK;
         color = CardColor.RED;
         rarity = CardRarity.COMMON;
         target = CardTarget.ENEMY;
         baseAttr = new BaseCardAttributes();
-        baseAttr.damage = 5;
-        baseAttr.block = 5;
+        baseAttr.damage = 3;
         usable = true;
         upgradable = true;
+        repeat = 3;
     }
 
     @Override
     public void use(ArrayList<AbstractMonster> monsters, AbstractCharacter hero) {
-        for (AbstractMonster m : monsters) {
-            m.changeHealth(-baseAttr.damage);
+        ArrayList<AbstractMonster> copy = Collections.shuffle(monsters);
+        for(int i = 0; i < repeat; i++) {
+            for (AbstractMonster m : copy) {
+                m.changeHealth(-baseAttr.damage);
+            }
         }
-        hero.changeShield(baseAttr.block);
         hero.currentEnergy -= cost;
     }
 
     @Override
     public void upgrade() {
-        if (upgradable) {
-            baseAttr.block = 7;
-            baseAttr.damage = 7;
+        if(upgradable){
             upgradable = false;
+            repeat = 4;
         }
     }
 
     @Override
-    public IronWave makeCopy() {
-        IronWave newCard = new IronWave();
+    public SwordBoomerang makeCopy() {
+        SwordBoomerang newCard = new SwordBoomerang();
         newCard.upgradable = this.upgradable;
-        newCard.baseAttr.damage = this.baseAttr.damage;
-        newCard.baseAttr.block = this.baseAttr.block;
+        newCard.repeat = this.repeat;
         return newCard;
     }
 }
