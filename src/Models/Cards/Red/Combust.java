fce8d09b9058eec3eb04f96ce059eaac86deade2
Ozge Yasayan <ozgey99@gmail.com>
2020-04-06 15:43:51 +0300

    new cards added

---
+++ src/Models/Cards/Red/Combust.java
@@ -0,0 +1,51 @@
+package Models.Cards.Red;
+
+import Models.Actions.AttackActions;
+import Models.Cards.*;
+import Models.Creatures.AbstractCharacter;
+import Models.Creatures.Monsters.AbstractMonster;
+import Models.Dungeon.Room.Fight;
+import Models.UI;
+
+public class Combust extends AbstractCard {
+
+    public Combust(){
+        name = "Combust";
+        description = "At the end of your turn, lose 1 HP and deal 5(7) damage to ALL enemies.";
+        cost = 1;
+        type = CardType.POWER;
+        color = CardColor.RED;
+        rarity = CardRarity.UNCOMMON;
+        target = CardTarget.ALL_ENEMY;
+        baseAttr = new BaseCardAttributes();
+        baseAttr.damage = 5;
+        usable = true;
+        upgradable = true;
+    }
+
+    @Override
+    public boolean use(Fight f, AbstractCharacter player) {
+        if (!player.changeEnergy(-cost)) return false;
+        for (AbstractMonster m : f.getMonsters()) {
+            m.changeHealth(-baseAttr.damage);
+        }
+        player.changeHealth(-1);
+        return true;
+    }
+
+    @Override
+    public void upgrade() {
+        if(upgradable){
+            upgradable = false;
+            baseAttr.damage = 7;
+        }
+    }
+
+    @Override
+    public Combust makeCopy() {
+        Combust newCard = new Combust();
+        newCard.upgradable = this.upgradable;
+        newCard.baseAttr.damage = this.baseAttr.damage;
+        return newCard;
+    }
+}
