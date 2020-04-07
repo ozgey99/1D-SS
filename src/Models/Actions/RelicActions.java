8e0fb160339b7708308adb8e608eaba5608ba8ae
Ozge Yasayan <ozgey99@gmail.com>
2020-04-07 16:32:07 +0300

    fixed addRelic function

--- src/Models/Actions/RelicActions.java
+++ src/Models/Actions/RelicActions.java
@@ -6,7 +6,8 @@ import Models.Object.AbstractRelic;
 public class RelicActions {
 
     public static void addRelic(AbstractCharacter c, AbstractRelic relic) {
-        c.relics.add(relic);
+        if(!c.relics.contains(relic))
+            c.relics.add(relic);
     }
     public static void removeRelic(AbstractCharacter c, AbstractRelic relic) {
         c.relics.remove(relic);
